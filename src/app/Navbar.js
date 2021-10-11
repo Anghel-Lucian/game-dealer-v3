01b32969f20e58b9640b46ec9a1c5f095d9d2492
Anghel-Lucian <anghelpaul99@gmail.com>
2021-10-11 09:26:12 +0300

    GD-54 added active state on route change for nav btns

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-  const currentPathname = useLocation();
-  console.log(currentPathname);
-
-  // const homePath = "/";
-  // const wishlistPath = "/wishlist";
-  // const libraryPath = "/library"
-
-  // const selectStylingClasses = (path) => {
-  //   if(path === currentPathname) {
-  //     return "navbar__link navbar__link--selected";
-  //   }
-
-  //   return "navbar__link"
-  // }
-
   return (
     <nav className="navbar">
       <div className="navbar__content">
-        <Link to="/" className="navbar__link navbar__link--selected">
+        <NavLink
+          to="/"
+          className="navbar__link"
+          activeClassName="navbar__link--selected"
+          exact
+        >
           <i className="fas fa-home" />
-        </Link>
-        <Link to="/wishlist" className="navbar__link">
+        </NavLink>
+        <NavLink
+          to="/wishlist"
+          className="navbar__link"
+          activeClassName="navbar__link--selected"
+          strict
+        >
           <i className="fas fa-gift" />
-        </Link>
-        <Link to="/library" className="navbar__link">
+        </NavLink>
+        <NavLink
+          to="/library"
+          className="navbar__link"
+          activeClassName="navbar__link--selected"
+          strict
+        >
           <i className="fas fa-layer-group" />
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
